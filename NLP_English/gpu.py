31cf267b07aa506d2248691581339eb7deb17fd1
AlexanderMargarhs <64739858+AlexanderMargarhs@users.noreply.github.com>
2022-06-05 20:23:47 +0300

    Delete NLP_English directory

--- NLP_English/gpu.py
+++
@@ -1,6 +0,0 @@
-# Simple script to check whether Tensorflow is using GPU or CPU
-import tensorflow as tf
-
-if __name__ == "__main__":
-	sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-	print(sess)
